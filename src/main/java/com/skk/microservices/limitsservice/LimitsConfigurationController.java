8e7e688e154a9fb1faf66d2c768158430acaac99
Kumar Kushwaha <s.a.kumar.kushwaha@accenture.com>
2018-11-30 15:45:18 +0530

    Added Falt tolerace

--- src/main/java/com/skk/microservices/limitsservice/LimitsConfigurationController.java
+++ src/main/java/com/skk/microservices/limitsservice/LimitsConfigurationController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.skk.microservices.limitsservice.bean.LimtConfiguration;
 
 @RestController
@@ -14,5 +15,15 @@ public class LimitsConfigurationController {
 	@GetMapping("/limits")
 	public LimtConfiguration retriveLimitsFromConfigurations() {
 		return new LimtConfiguration(configuration.getMaximum(),configuration.getMinimum());
+	}
+	
+	@GetMapping("/hystrix")
+	@HystrixCommand(fallbackMethod="fallback")
+	public LimtConfiguration retriveConfigurations() {
+		throw new RuntimeException("This service is not available");
+	}
+	
+	public LimtConfiguration fallback() {
+		return new LimtConfiguration(10, 10);
 	}
 }
